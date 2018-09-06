8b0a5100da76d40a497e351407c7dbc3009dd042
histnat <histoire.naturelle@hotmail.fr>
2018-09-06 17:22:53 +0200

    1er commit

---
+++ zuul-server/src/main/java/net/rebeche/mithrarugby/zuulserver/filters/LogFilter.java
@@ -0,0 +1,45 @@
+package net.rebeche.mithrarugby.zuulserver.filters;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
+import com.netflix.zuul.exception.ZuulException;
+
+@Component
+public class LogFilter extends ZuulFilter {
+
+	Logger log = LoggerFactory.getLogger(this.getClass());
+	
+	@Override
+	public Object run() throws ZuulException {
+		HttpServletRequest req= RequestContext.getCurrentContext().getRequest();
+		
+		log.info("*** Requete interceptée "+req.getRequestURL());
+		
+		return null;
+	}
+
+	@Override
+	public boolean shouldFilter() {
+		// TODO Auto-generated method stub
+		return true;
+	}
+
+	@Override
+	public int filterOrder() {
+		// TODO Auto-generated method stub
+		return 1;
+	}
+
+	@Override
+	public String filterType() {
+		// TODO Auto-generated method stub
+		return "pre";
+	}
+
+}

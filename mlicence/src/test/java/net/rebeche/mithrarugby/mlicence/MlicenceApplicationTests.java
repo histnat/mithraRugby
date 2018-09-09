4676157ca6655bd463483428a1f7daee2b7670c6
histnat <histoire.naturelle@hotmail.fr>
2018-09-09 17:39:01 +0200

    mlicence

---
+++ mlicence/src/test/java/net/rebeche/mithrarugby/mlicence/MlicenceApplicationTests.java
@@ -0,0 +1,16 @@
+package net.rebeche.mithrarugby.mlicence;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MlicenceApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
